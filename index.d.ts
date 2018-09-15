2bb73db05800253637fd375a9d52349d1610cbad
kristoffer petersen <krizzmp@gmail.com>
2018-09-15 15:46:01 +0200

    update Text to inherit from baseNode

--- index.d.ts
+++ index.d.ts
@@ -292,7 +292,7 @@ export interface LeafAndOffset {
     leaf: Leaf;
 }
 
-export class Text extends Immutable.Record({}) {
+export class Text extends BaseNode {
     object: "text";
     key: string;
 
@@ -315,7 +315,6 @@ export class Text extends Immutable.Record({}) {
     getKeysToPathsTable(): object;
     getString(): string;
     searchLeafAtOffset(offset: number): LeafAndOffset;
-    addMark(index: number, length: number, mark: Mark): Text;
     addMarks(index: number, lenght: number, marks: Immutable.Set<Mark>): Text;
     getLeaves(decorations?: Range[]): Immutable.List<Leaf>;
     getActiveMarksBetweenOffset(
@@ -336,10 +335,7 @@ export class Text extends Immutable.Record({}) {
     getNode(key: string): Node | null;
     getPath(key: string | Path): Path;
     hasNode(key: string): boolean;
-    insertText(offset: number, text: string, marks?: Immutable.Set<Mark>): Text;
     regenerateKey(): Text;
-    removeMark(index: number, length: Mark, mark: Mark): Text;
-    removeText(start: number, length: number): Text;
     resolvePath(path: Path, index: number): Immutable.List<string>;
     updateMark(
         index: number,
